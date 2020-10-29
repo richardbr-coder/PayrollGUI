3be8ce1d33c2c529133db21a74ac5830c4ad0c1f
Richard <52008509+richardbr-coder@users.noreply.github.com>
2020-10-29 15:32:35 -0230

    Add files via upload

---
+++ PayrollGUI/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace PayrollGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.txtPayRate = new System.Windows.Forms.TextBox();
+            this.txtHoursWorked = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter Your name:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Social security number:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Hourly pay rate:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Hours worked:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 218);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 38);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(180, 29);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(153, 20);
+            this.txtName.TabIndex = 9;
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(180, 71);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(153, 20);
+            this.txtNumber.TabIndex = 10;
+            // 
+            // txtPayRate
+            // 
+            this.txtPayRate.Location = new System.Drawing.Point(180, 112);
+            this.txtPayRate.Name = "txtPayRate";
+            this.txtPayRate.Size = new System.Drawing.Size(80, 20);
+            this.txtPayRate.TabIndex = 11;
+            // 
+            // txtHoursWorked
+            // 
+            this.txtHoursWorked.Location = new System.Drawing.Point(180, 154);
+            this.txtHoursWorked.Name = "txtHoursWorked";
+            this.txtHoursWorked.Size = new System.Drawing.Size(80, 20);
+            this.txtHoursWorked.TabIndex = 12;
+            // 
+            // lblResult
+            // 
+            this.lblResult.Location = new System.Drawing.Point(180, 218);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(347, 232);
+            this.lblResult.TabIndex = 13;
+            // 
+            // lblClear
+            // 
+            this.lblClear.Location = new System.Drawing.Point(93, 218);
+            this.lblClear.Name = "lblClear";
+            this.lblClear.Size = new System.Drawing.Size(59, 38);
+            this.lblClear.TabIndex = 15;
+            this.lblClear.Text = "CLEAR";
+            this.lblClear.UseVisualStyleBackColor = true;
+            this.lblClear.Click += new System.EventHandler(this.lblClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 430);
+            this.Controls.Add(this.lblClear);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtHoursWorked);
+            this.Controls.Add(this.txtPayRate);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Payroll Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.TextBox txtPayRate;
+        private System.Windows.Forms.TextBox txtHoursWorked;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button lblClear;
+    }
+}
+
---
+++ PayrollGUI/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PayrollGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Added new .ico image for on the taskbar and open window 
+            //Variables
+            decimal totGPay, fedHeld, statHeld, netPay;
+
+            //converting
+            decimal perFedHeld = Convert.ToDecimal(.15);
+            decimal perStatHeld = Convert.ToDecimal(.05);
+            decimal payRate = Convert.ToDecimal(txtPayRate.Text);
+            decimal hrsWork = Convert.ToDecimal(txtHoursWorked.Text);
+
+            //calculations
+            totGPay = Math.Round(payRate * hrsWork, 2);
+            fedHeld = Math.Round(totGPay * perFedHeld, 2);
+            statHeld = Math.Round(totGPay * perStatHeld, 2);
+            netPay = Math.Round(totGPay - (fedHeld + statHeld), 2);
+            
+            /*I was going to make 3-4 different labels to display the information to the user
+            but thought one label with line breaks would be more efficent in this case*/
+            lblResult.Text = string.Format("Payroll Summary for: " + txtName.Text + "   SSN: " + txtNumber.Text +
+            "\nYou worked " + txtHoursWorked.Text + " hours @ $" + txtPayRate.Text +
+            "\n\n\nGross Pay:       $" + totGPay +
+            "\n\n\nFederal withholding:   $" + fedHeld +
+            "\nState withholdings:    $" + statHeld +
+            "\n--------------------------------------------\n" +
+            "Net Pay:       $" + netPay);
+
+            
+        }
+
+        //I just watched the first matrix movie, again! lol So I call this my "fresh start protocol" (The red pill) :)
+        private void lblClear_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = ""; 
+            txtHoursWorked.Text = String.Empty; 
+            txtName.Text = String.Empty; 
+            txtNumber.Text = String.Empty; 
+            txtPayRate.Text = String.Empty;
+        }
+
+
+    }
+}
